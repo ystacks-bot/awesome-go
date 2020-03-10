137fadaf9cfa0a7f43ebaa4e63bd7bc20a8dcbd5
ystacks-inc <61998983+ystacks-inc@users.noreply.github.com>
2020-03-10 12:08:19 +0800

    update output from forked

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example with anchore container compliance engine with latest jx"
+	title := "Jenkins X golang http forked-v1"
 
 	from := ""
 	if r.URL != nil {
